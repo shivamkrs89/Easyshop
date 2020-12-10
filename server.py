62e790f1f558088435343aa0cb2e88354d345654
shivam kumar <imshivamkr2@gmail.com>
2020-12-10 12:21:46 +0530

    email from first part of seller registration is sent to 2nd via session

--- server.py
+++ server.py
@@ -1,37 +1,36 @@
 from flask import Flask, request, session, render_template, flash, redirect, url_for
-
+import datetime
 from common import dblogin
+app = Flask(__name__, template_folder='template/')
+app.secret_key = 'the random string'
+app.permanent_session_lifetime = datetime.timedelta(days=1)
+
 
 @app.route('/signup_shop1', methods=["POST", "GET"])
 def shop1_sign_up():
     if request.method == "POST":
         form_data = request.form.to_dict()
-        fname = form_data['fname']
-        lname = form_data['lname']
-        passwd = form_data['pass']
-        email = form_data['email']
-        passwdchk = form_data['passcheck']
-        address = form_data['address']
-        phno = str(form_data['ph_no']
+        # fname = form_data['fname']
+        # lname = form_data['lname']
+        # passwd = form_data['pass']
+        # email = form_data['email']
+        # passwdchk = form_data['passcheck']
+        # addr = form_data['address']
+        # phno = str(form_data['ph_no']
+        fname=form_data['fname']
+        lname=form_data['lname']
+        passwd=form_data['pass']
+        email=form_data['email']
+        passwdchk=form_data['passcheck']
+        address=form_data['address']
+        phno=form_data['ph_no']
 
-        if fname == '':
-            flash("Please enter your first name")
-            return render_template('signup_shop1.html')
-        elif address == '':
-            flash("Please enter address")
-            return render_template('signup_shop1.html')
-        elif phno == '':
-            flash("Please enter phno")
-            return render_template('signup_shop1.html')
-        elif passwd == '':
-            flash("Please enter a password")
-            return render_template('signup_shop1.html')
-        elif passwdchk == '':
-            flash("Please enter confirm password")
-            return render_template('signup_shop1.html')
-        elif passwd != passwdchk:
-            flash("Password and confirm password do not match, Please re-enter")
-            return render_template('signup_shop1.html')
+        if passwd != passwdchk:
+            flash("Password doesn't match!")
+            render_template('signup_shop1.html')
+
+        if session[email]==0:
+            session[emaiid]=email
 
         success=dblogin.seller_registration1(fname, lname, email, address, passwd, phno)
 
@@ -51,6 +50,7 @@ def sign_up_shop2():
         sname=form_data['snmae']
         saddress = form_data['saddress']
         stype=form_data['stype']
+        email=session.get('emailid')
         if sname == '':
             flash("Please enter your shop name")
             return render_template('signup_shop2.html')
@@ -60,10 +60,14 @@ def sign_up_shop2():
         elif stype == '':
             flash("Please specifythe category of the shop")
             return render_template('signup_shop2.html')
-        success=dblogin.seller_registration2(sname,stype,saddress)
+        success=dblogin.seller_registration2(email,sname,stype,saddress)
         if success == 0:
             flash("same shop name already exists, try different")
             return render_template('signup_shop2.html')
         elif success == 1:
             return render_template('signin.html')
     return render_template('signup_shop1.html')
+
+
+if __name__ == "__main__":
+    app.run(debug="true")
